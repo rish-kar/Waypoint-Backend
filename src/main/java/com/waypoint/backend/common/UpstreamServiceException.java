a43a7a09383df11aeb010e1ecd4c64e46fd21676
Rishabh Kar <46086520+rish-kar@users.noreply.github.com>
2026-07-29 17:18:10 +0100

    Add authentication provider availability error

---
+++ src/main/java/com/waypoint/backend/common/UpstreamServiceException.java
@@ -0,0 +1,9 @@
+package com.waypoint.backend.common;
+
+import org.springframework.http.HttpStatus;
+
+public class UpstreamServiceException extends ApiException {
+    public UpstreamServiceException(String message) {
+        super(HttpStatus.SERVICE_UNAVAILABLE, "AUTH_PROVIDER_UNAVAILABLE", message);
+    }
+}
