b1b6d8c74bfe57618db3300dbf9fbd338577e715
Rishabh Kar <46086520+rish-kar@users.noreply.github.com>
2026-08-18 15:21:38 +0100

    Fix duplicate Flyway index migration

--- src/main/resources/db/migration/V13__add_hot_query_indexes.sql
+++ src/main/resources/db/migration/V13__add_hot_query_indexes.sql
@@ -13,9 +13,6 @@ CREATE INDEX idx_subscriptions_user_status_ends_at
 CREATE INDEX idx_webhook_events_status_last_attempt_at
     ON webhook_events (processing_status, last_attempt_at);
 
-CREATE INDEX idx_admin_audit_events_created_at
-    ON admin_audit_events (created_at DESC);
-
 CREATE INDEX idx_admin_audit_events_admin_created_at
     ON admin_audit_events (admin_id, created_at DESC);
 
