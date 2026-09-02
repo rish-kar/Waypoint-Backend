75fd46394825f9dd24d57f714c30f45aa2750a31
Rishabh Kar <46086520+rish-kar@users.noreply.github.com>
2026-09-02 21:57:15 +0530

    Make Family AI migration follow existing V22

--- src/main/resources/db/migration/V23__move_family_ai_usage_to_special_grants.sql
+++ src/main/resources/db/migration/V23__move_family_ai_usage_to_special_grants.sql
@@ -1,5 +1,2 @@
 ALTER TABLE special_premium_grants ADD COLUMN ai_period_key VARCHAR(7);
 ALTER TABLE special_premium_grants ADD COLUMN ai_spent_microrupees BIGINT NOT NULL DEFAULT 0;
-
-DROP TABLE family_ai_user_usage;
-DROP TABLE family_ai_pool_usage;
